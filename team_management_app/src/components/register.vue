8b9243fbee32860735c422e9f0cd61362d0cd06f
louis3546 <lourensvdbijl@gmail.com>
2021-05-30 18:15:59 +0200

    Update register.vue

--- team_management_app/src/components/register.vue
+++ team_management_app/src/components/register.vue
@@ -1,20 +1,8 @@
 <template>
   <!--Registration -->
-  <form>
-   <div class="form-group">
-    <label for="email" class="readText">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" required>
-    <br>
-  </div>
-  <div class="form-group">
-    <label for="password" class="readText">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" required>
-  </div>
-  <div class="form-check">
-    <br>
-  </div>
-  <button type="submit" class="btn btn-primary">Login</button>
-</form>
+ <div>
+   Helow from registrationPage
+ </div>
 
 
 
